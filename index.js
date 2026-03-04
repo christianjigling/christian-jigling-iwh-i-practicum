69c37b0ccc4213c547fa01a217b5fa94a0ed785b
christianjigling <christian.jigling@leadfront.com>
2026-03-04 10:56:28 +0100

    Homepage Pug tempalte + routing added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,28 +9,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
+app.get('/', async (req, res) => {
+    const catsUrl = 'https://api.hubspot.com/crm/v3/objects/2-199369689?properties=name,breed,age';
+        const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
-    }
+    };
+
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(catsUrl, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        console.log(data);
+        res.render('homepage', { title: 'Cats | HubSpot APIs', data });      
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching cats:', error.response ? error.response.data : error.message);
+        res.send('Something went wrong when fetching the cats.');
     }
 });
-// * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
+app.get('/update-cobj', async (req, res) => {
+    const catsUrl = 'https://api.hubspot.com/crm/v3/objects/cats?properties=name,breed,age'; 
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(catsUrl, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Cats | HubSpot APIs', data });      
+    } catch (error) {
+        console.error('Error fetching cats:', error.response ? error.response.data : error.message);
+        res.send('Something went wrong when fetching the cats.');
+    }
+});
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
